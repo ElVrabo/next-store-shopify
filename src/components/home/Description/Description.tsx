57b54c943ec91956aed80faef03245f2c0a77d10
Brandonmkmk <brandonxd611@gmail.com>
2024-03-20 15:02:47 -0600

    13 optimizacion del componente image

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -4,7 +4,16 @@ export function Description(){
     
     return (
         <section className={styles.Description} >
-            <Image src="/images/description.jpeg" alt="products marketplace" priority={false} quality={30}  width={300} height={300} />
+            <div className={styles.Description_imageContainer} >
+
+            <Image
+             src="/images/description.jpeg" 
+             alt="products marketplace" 
+             width={500}
+             height={300}
+              />
+            </div>
+            
             <div className={styles.Description_text} >
                 <h2>Bring the future today</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui illo amet ipsa reprehenderit neque cum, iusto incidunt totam eos asperiores repellendus libero perspiciatis accusamus quos expedita modi adipisci delectus labore?</p>
