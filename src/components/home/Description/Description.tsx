5b1f91144069bfead55ad911b6c1062fc854ae38
Brandonmkmk <brandonxd611@gmail.com>
2024-03-20 14:30:27 -0600

    12 manejo y optimizacion de imagenes

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image"
+import styles from "./Description.module.sass"
 export function Description(){
+    
     return (
-        <section>
-            <img src="/images/description.jpeg" alt="products marketplace" />
-            <h2>Description</h2>
-            <p>Descripcion de los productos</p>
+        <section className={styles.Description} >
+            <Image src="/images/description.jpeg" alt="products marketplace" priority={false} quality={30}  width={300} height={300} />
+            <div className={styles.Description_text} >
+                <h2>Bring the future today</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui illo amet ipsa reprehenderit neque cum, iusto incidunt totam eos asperiores repellendus libero perspiciatis accusamus quos expedita modi adipisci delectus labore?</p>
+            </div>
+            
         </section>
     )
 }
