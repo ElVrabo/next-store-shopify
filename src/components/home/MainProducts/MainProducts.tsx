7de5eccc05e54345243e9fcd8cf33b9bd2f1d4cf
Brandonmkmk <brandonxd611@gmail.com>
2024-03-21 12:02:07 -0600

    16 obteniendo informacion de la api

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,6 +1,18 @@
 
-export function MainProducts(){
-    console.log(process.env.SHOPIFY_HOSTNAME)
+const getProducts = async ()=>{
+    const res = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,{
+        headers: new Headers({
+            'x-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+        })
+    })
+    const data = await res.json()
+    return data
+}
+export async function MainProducts(){
+
+    const products = await getProducts()
+    console.log(products)
+    
     return (
         <section>
             <h1>Main Products</h1>
