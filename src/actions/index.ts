152a99fae806f58f397adca3c34adc9d552dbe5d
Brandonmkmk <brandonxd611@gmail.com>
2024-04-18 22:09:45 -0600

    Seccion 5: manejo de cookies para token de acceso

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,17 +1,24 @@
 'use server'
 import { GraphQLClientSingleton } from "app/graphql"
 import { createUserMutation } from "app/graphql/mutations/createUserMutation"
+import { createAccesToken } from "app/utils/auth/createAccesToken"
+import { redirect } from "next/navigation"
 
 export  const handleCreateUser = async (formData:any)=>{
     const formDataObject = Object.fromEntries(formData)
-    delete formDataObject["password_condition"]
+    delete formDataObject["password_confirmation"]
 const graphqlClient =  GraphQLClientSingleton.getInstance().getClient()
-const data = await graphqlClient.request(createUserMutation,{
-})
+
 const variables = {
     input:{
       ...formDataObject,
       phone: "+52" + formDataObject.phone
     }
+}
+const {customerCreate} = await graphqlClient.request(createUserMutation,variables)
+const {customerUserErrors,customer} = customerCreate
+if(customer?.firstName){
+await createAccesToken(formDataObject.email, formDataObject.password)
+redirect("/store")
 }
 }
